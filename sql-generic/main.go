ab60b249d36fdd028dea154b173adb70f1c2f222
Parlindungan375 <psitanggang49@gmail.com>
2020-10-22 21:49:00 +0700

    first commit

---
+++ sql-generic/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/Parlindungan375/dts-pengenalan-database-1/sql-generic/config"
+	"github.com/Parlindungan375/dts-pengenalan-database-1/sql-generic/database"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	cfg, err := getConfig()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	db, err := connect(cfg.Database)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	//database.InsertCustomer(database.Customer{
+	//	FirstName:    "Sabar",
+	//	LastName:     "Parlindungan",
+	//	NpwpId: "id-1",
+	//	Age:          10,
+	//	CustomerType: "Premium",
+	//	Street:       "Str",
+	//	City:         "Yogya",
+	//	State:        "Indo",
+	//	ZipCode:      "55555",
+	//	PhoneNumber:  "0812384",
+	//},db)
+
+	//database.GetCustomers(db)
+	//database.DeleteCustomer(1,db)
+	database.UpdateCustomer(30, 2, db)
+}
+
+func getConfig() (config.Config, error) {
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yml")
+	viper.SetConfigName("config.yml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return config.Config{}, err
+	}
+
+	var cfg config.Config
+	err := viper.Unmarshal(&cfg)
+	if err != nil {
+		return config.Config{}, err
+	}
+
+	return cfg, nil
+}
+
+func connect(cfg config.Database) (*sql.DB, error) {
+	db, err := sql.Open(cfg.Driver, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?%s", cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DbName, cfg.Config))
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("db successfully connected")
+	return db, nil
+}
